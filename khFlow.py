777cac0ebcb2031fa877fae81c5e6d7f1de654bf
pavelpetrcz <ppetr@ppetr.cz>
2021-12-22 21:55:07 +0100

    renaming variables

--- khFlow.py
+++ khFlow.py
@@ -22,7 +22,7 @@ def execute(numMonth, year, khOutputLoc, khTempFile):
     expenses = pd.read_excel("A://OSVC//dph//evidence.xlsx", sheet_name="vydaje", engine='openpyxl')
 
     # Changes in XML template
-    # VetaD (mesic - param mesic, rok - rok, datum vystavení - d_poddp)
+    # VetaD (datum vystavení - d_poddp)
     datum_podani = commonMethods.getTodayDateAsString()
 
     DPHKH1.find("VetaD").set("mesic", str(numMonth))
@@ -31,18 +31,18 @@ def execute(numMonth, year, khOutputLoc, khTempFile):
 
     # Incomes
     # VetaA4+A5
-    num = 1
-    obrat = 0
-    dan = 0
-    obrat15 = 0
-    dan15 = 0
+    in_row_number = 1
+    in_castka_bez_dph21 = 0
+    in_castka_dph21 = 0
+    in_castka_bez_dph_15 = 0
+    in_castka_dph15 = 0
     for index, item in incomes.iterrows():
         if item["mesic"] == int(numMonth) and item["rok"] == int(year):
             if not pd.isna(item["dic"]):
-                obrat = obrat + item["castka_bez_dph"]
-                dan = dan + item["dph"]
+                in_castka_bez_dph21 = in_castka_bez_dph21 + item["castka_bez_dph"]
+                in_castka_dph21 = in_castka_dph21 + item["dph"]
                 A4 = et.SubElement(root.find("DPHKH1"), 'VetaA4')
-                A4.set('c_radku', str(num))
+                A4.set('c_radku', str(in_row_number))
                 A4.set('dic_odb', str(int(item["dic"])))
                 A4.set('c_evid_dd', str(item["cisloDokladu"]))
                 A4.set('dppd', str(item["duzp"].strftime("%d.%m.%Y")))
@@ -50,36 +50,45 @@ def execute(numMonth, year, khOutputLoc, khTempFile):
                 A4.set('dan1', str(round(item["dph"])))
                 A4.set('kod_rezim_pl', '0')
                 A4.set('zdph_44', 'N')
-                num = num + 1
+                in_row_number = in_row_number + 1
             else:
-                obrat15 = obrat15 + item["castka_bez_dph"]
-                dan15 = dan15 + item["dph"]
+                in_castka_bez_dph_15 = in_castka_bez_dph_15 + item["castka_bez_dph"]
+                in_castka_dph15 = in_castka_dph15 + item["dph"]
     A5 = et.SubElement(root.find("DPHKH1"), 'VetaA5')
-    A5.set('zakl_dane2', str(round(obrat15)))
-    A5.set('dan2', str(round(dan15)))
+    A5.set('zakl_dane2', str(round(in_castka_bez_dph_15)))
+    A5.set('dan2', str(round(in_castka_dph15)))
 
     # Expenses
     # VetaB3+B2
-    dan_21 = 0
-    zaklad_dane_21 = 0
     numB2 = 1
-    dan_snizena = 0
-    zakl_dane_snizena = 0
+
+    ex_dph_celkem_21 = 0
+    ex_castka_bez_dph_celkem_21 = 0
+
+    ex_dph_celkem_15 = 0
+    ex_castka_bez_dph_celkem_15 = 0
+
+    ex_dph_celkem_10 = 0
+    ex_castka_bez_dph_celkem_10 = 0
+
     for index, item in expenses.iterrows():
         if item["mesic"] == int(numMonth) and item["rok"] == int(year):
-            castka_bez_dph = item["castka_bez_dph"]
-            castka_celkem = item['castka_celkem']
-            dan = item["dan"]
-            dph = item["dph"]
+            ex_castka_bez_dph = item["castka_bez_dph"]
+            ex_castka_celkem = item['castka_celkem']
+            ex_sazba = item["sazba"]
+            ex_castka_dph = item["dph"]
 
-            if castka_celkem < 10000 and dan == 21:
-                zaklad_dane_21 = zaklad_dane_21 + castka_bez_dph
-                dan_21 = dan_21 + dph
-            elif castka_celkem < 10000 and (dan == 10 or dan == 15):
-                zakl_dane_snizena = + castka_bez_dph
-                dan_snizena = + dph
-            elif castka_celkem >= 10000:
-                if dan == 21:
+            if ex_castka_celkem < 10000 and ex_sazba == 21:
+                ex_castka_bez_dph_celkem_21 = ex_castka_bez_dph_celkem_21 + ex_castka_bez_dph
+                ex_dph_celkem_21 = ex_dph_celkem_21 + ex_castka_dph
+            elif ex_castka_celkem < 10000 and ex_sazba == 15:
+                ex_castka_bez_dph_celkem_15 = + ex_castka_bez_dph
+                ex_dph_celkem_15 = + ex_castka_dph
+            elif ex_castka_celkem < 10000 and ex_sazba == 10:
+                ex_castka_bez_dph_celkem_10 = + ex_castka_bez_dph
+                ex_dph_celkem_10 = + ex_castka_dph
+            elif ex_castka_celkem >= 10000:
+                if ex_sazba == 21:
                     B2 = et.SubElement(root.find("DPHKH1"), 'VetaB2')
                     B2.set('c_radku', str(numB2))
                     B2.set('dic_dod', str(item["dic"]))
@@ -90,7 +99,7 @@ def execute(numMonth, year, khOutputLoc, khTempFile):
                     B2.set('pomer', 'N')
                     B2.set('zdph_44', 'N')
                     numB2 = + numB2
-                elif dan == 10 or dan == 15:
+                elif ex_sazba == 10 or ex_sazba == 15:
                     logging.warning(
                         'Setting VetaB2 - zaklad dane je vice jak 10000 a zaroven dan 10 nebo 15, neumim spravne vytvorit VetuB2')
                     raise ValueError(
@@ -101,23 +110,25 @@ def execute(numMonth, year, khOutputLoc, khTempFile):
             else:
                 continue
     B3 = et.SubElement(root.find("DPHKH1"), 'VetaB3')
-    B3.set('zakl_dane1', str(round(zaklad_dane_21)))
-    B3.set('dan1', str(round(dan_21)))
-    B3.set('zakl_dane3', str(zakl_dane_snizena))
-    B3.set('dan3', str(dan_snizena))
+    B3.set('zakl_dane1', str(round(ex_castka_bez_dph_celkem_21)))
+    B3.set('dan1', str(round(ex_dph_celkem_21)))
+    B3.set('zakl_dane3', str(ex_castka_bez_dph_celkem_10))
+    B3.set('dan3', str(ex_dph_celkem_10))
+    B3.set('dan2', str(round(ex_dph_celkem_15)))
+    B3.set('zakl_dane2', str(round(ex_castka_bez_dph_celkem_15)))
 
     # VetaC
     C = et.SubElement(root.find("DPHKH1"), 'VetaC')
-    C.set('obrat23', str(round(obrat)))
-    C.set('pln23', str(round(zaklad_dane_21)))
-    C.set('pln5', str(zakl_dane_snizena))
+    C.set('obrat23', str(round(in_castka_bez_dph21)))
+    C.set('pln23', str(round(ex_castka_bez_dph_celkem_21)))
+    C.set('pln5', str(ex_castka_bez_dph_celkem_15 + ex_castka_bez_dph_celkem_10))
 
     file_name = "kh_output.xml"
     head = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>"
     completeName = os.path.join(khOutputLoc, file_name)
 
-    ktData = et.tostring(root)
+    khData = et.tostring(root)
 
     with open(completeName, "w") as f:
         f.write(head)
-        f.write(str(ktData.decode("utf-8")))
+        f.write(str(khData.decode("utf-8")))
